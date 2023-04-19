73f79494ddb51a294b9f5b875c159bd97817c7fe
XxCheezburgerzxX <spencerng14@gmail.com>
2023-04-19 21:39:21 +0000

    learning about input function

---
+++ Lab1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# print question
+name = input("What is your name?: ")
+
+day = input("What day is it?: ")
+
---
+++ lab_input/Challenge_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | AOViper
+   print() - display data to std out"""
+
+def main():
+
+    user_input1 = input("What is your name?: ")
+
+    User_input2 = input(What day of the week is it?: ")
+
+    ## the line below creates a single string that is passed to print()
+    # print("Hello, " user_input1, "Happy " user_input2 ")
+
+main()
